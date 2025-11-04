6d707eeb30dd023c03204f66722ee40fcfc5d1e5
Logm12 <99984683+Logm12@users.noreply.github.com>
2025-11-04 14:15:56 +0700

    Update build_dim_customer.sql

--- dags/sql/transform/build_dim_customer.sql
+++ dags/sql/transform/build_dim_customer.sql
@@ -12,8 +12,8 @@ SELECT DISTINCT
     customer_fname,
     customer_lname,
     customer_segment,
-    -- Dùng thông tin geography từ Bảng Orders (giả định là nơi ở của KH)
     order_city AS customer_city,
     order_state AS customer_state,
     order_country AS customer_country
+
 FROM staging.stg_oms_orders;
