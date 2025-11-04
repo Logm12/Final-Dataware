a8b804a493f1462001315705e2c71562929fc31d
Logm12 <99984683+Logm12@users.noreply.github.com>
2025-11-04 14:17:25 +0700

    Update truncate_dwh_tables.sql

--- dags/sql/transform/truncate_dwh_tables.sql
+++ dags/sql/transform/truncate_dwh_tables.sql
@@ -1,9 +1,3 @@
-/*
-Dọn dẹp TOÀN BỘ DWH trước khi build.
-RESTART IDENTITY: Reset các cột SERIAL (xxx_key) về 1.
-CASCADE: Tự động xóa các Foreign Key đang trỏ đến (từ bảng Fact),
-         cho phép TRUNCATE các bảng Dim.
-*/
 TRUNCATE 
     dwh.dim_customer,
     dwh.dim_product,
@@ -13,4 +7,5 @@ TRUNCATE
     dwh.dim_geography,
     dwh.dim_shipping,
     dwh.order_items
+
 RESTART IDENTITY CASCADE;
